63bc219b921dec55003e4cedd40768d59e61aad6
4hmetuyar <4hmetuyar@gmail.com>
2019-07-30 16:38:27 +0300

    Referances,our services, contact us page added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ReferancesComponent } from './referances/referances.component';
+import { OurServicesComponent } from './our-services/our-services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    ReferancesComponent,
+    OurServicesComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/referances/referances.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-referances',
+  templateUrl: './referances.component.html',
+  styleUrls: ['./referances.component.css']
+})
+export class ReferancesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
